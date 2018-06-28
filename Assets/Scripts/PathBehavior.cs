10ad081c345e1c10381f086d4f3323bbaed0ea3a
Anden Acitelli <a.acitelli@gmail.com>
2018-06-28 16:29:25 -0400

    Initial Commit

---
+++ Assets/Scripts/PathBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBehavior : MonoBehaviour
+{
+    private float lastXPos;
+
+    // Initialization of Variables
+    private void Start()
+    {
+        lastXPos = 0;
+    }
+
+    // Called once per frame
+    private void Update()
+    {        
+        // Moves downward slowly - This works fine
+        Vector3 position = this.transform.position;
+        position.y = this.transform.position.y - .05f;
+        this.transform.position = position;        
+    }
+    
+    // Called in time intervals, not per frame - Use this for physics stuff so it looks consistent regardless of lag
+    private void FixedUpdate()
+    {
+        ReuseObject();
+    }
+
+    // Makes a 
+    public static void CreateInitialObjects(float yPos)
+    {
+        Vector3 newPos;
+        
+        // Actually making the new object
+        newPos = new Vector3(0, yPos, 90);        
+        Instantiate((GameObject)Resources.Load("PathSprite"), newPos, Quaternion.identity);               
+    }
+    
+    // Checks if it is below the camera, and if so, puts it back up top with a new x-coordinate that makes sense so the whole thing isn't straight
+    private void ReuseObject()
+    {
+       if (transform.position.y < -5)
+        {
+            Vector3 position = this.transform.position;
+            position.y = 5f;
+            position.x = lastXPos + Random.Range(-.3f, .3f);
+            this.transform.position = position;
+
+            lastXPos = position.x;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private void Start()
+    {
+        for (float y = 5; y < 15; y += .125f)
+        {
+            PathBehavior.CreateInitialObjects(y - 10);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If they have the "left" key pressed
+        if (Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > -9.8f)
+        {
+            Vector3 position = this.transform.position;
+            position.x -= .2f;
+            this.transform.position = position;
+        }
+
+        // If they have the "right" key pressed
+        if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < 9.8f)
+        {
+            Vector3 position = this.transform.position;
+            position.x += .2f;
+            this.transform.position = position;
+        }
+        
+        // If I get in any infinite loops by accident, this will hopefully work
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
