531b439fcf4bb5f6591796dadfcba4a39ffd4a0a
Anden Acitelli <a.acitelli@gmail.com>
2018-06-28 20:12:24 -0400

    Fixed Path Generation, Started Collision

--- Assets/Scripts/PathBehavior.cs
+++ Assets/Scripts/PathBehavior.cs
@@ -1,14 +1,11 @@
-﻿/* Todo - 
- *      I think my current implementation means that the last x-coord is saved specific to each section of the path. 
- *          - Will have to figure out how to fix that.
- *      */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PathBehavior : MonoBehaviour
 {
+    const float EASY_DIFFICULTY = .06f, MEDIUM_DIFFICULTY = .08f, HARD_DIFFICULTY = .1f, TESTING_DIFFICULTY = .4f;
+
     // Initialization of Variables
     private void Start()
     {
@@ -20,14 +17,18 @@ public class PathBehavior : MonoBehaviour
     {        
         // Moves downward slowly - This works fine
         Vector3 position = this.transform.position;
-        position.y = this.transform.position.y - .25f;
+
+        // Todo - Implement time elapsed into this
+        position.y = this.transform.position.y - HARD_DIFFICULTY;
         this.transform.position = position;        
     }
     
     // Called in time intervals, not per frame - Use this for physics stuff so it looks consistent regardless of lag
     private void FixedUpdate()
     {
+        // Checks if they go off the bottom of the screen and sends them back up top
         ReuseObject();
+        CheckCollision();
     }
 
     // Makes a 
@@ -43,15 +44,38 @@ public class PathBehavior : MonoBehaviour
     // Checks if it is below the camera, and if so, puts it back up top with a new x-coordinate that makes sense so the whole thing isn't straight
     private void ReuseObject()
     {
-       // Resets it back to the top of the screen
-       if (transform.position.y < -6)
-        {
+       // Resets it back to the top of the screen - Only does it if no path has already moved back up top
+       if (transform.position.y < -6 && !PlayerMovement.pathHasMovedThisFrame)
+       {
+            PlayerMovement.pathHasMovedThisFrame = true;
+
             Vector3 position = this.transform.position;
             position.y = 6f;
             position.x = PlayerMovement.pathLastXValue + Random.Range(-.4f, .4f);
+
+            // These two lines are to make sure the path never generates off of the screen. Works by resetting the value to closer inside the screen, but offset by a small random value so it doesn't just show up as a straight line
+            if (position.x >= 10)
+                position.x = 10 + Random.Range(-.1f, .1f);
+            else if (position.x <= -10)
+                position.y = -10 + Random.Range(-.1f, .1f);
+
+            // If everything checks out, the position of the sprite is set
             this.transform.position = position;
 
             PlayerMovement.pathLastXValue = position.x;
+        }
+    }
+
+    private void CheckCollision()
+    {
+        // Checking Y-coordinate first because that will rule out more than once and be slightly more efficient
+        if (this.transform.position.y <= -2.4 && this.transform.position.y >= 3.6)
+        {
+            // Checking that the player is close enough to the middle of all the platforms at the current y-coordinate
+            if (!(Mathf.Abs(this.transform.position.x - PlayerMovement.xCoord) <= 3.5f))
+            {
+                Application.Quit(); // Todo - Make this take you back to a menu or something
+            }
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // This value needs to be the same across every path segment, and this is the only class that is the same across all of them
-    public static float pathLastXValue;
+    // Public Variables
+    public static float pathLastXValue;    // This value needs to be the same across every path segment, and this is the only class that is the same across all of them
+    public static float xCoord; // Used by the path class to check for collision - Doing it the non-Unity way because unity doesn't really have what I want
+    public static bool pathHasMovedThisFrame = false;
 
+    // Private Variables
+    private int count = 0;
+    
+    // Run once at the start of the game
     private void Start()
     {
-        for (float y = 4; y < 16; y += .125f)
+        // Makes quite a few extras to account for higher speeds
+        // Todo - Make this make different amounts for each of the difficulties. Don't forget that it gets faster as time goes on, or at least is supposed to
+        for (float y = 4; y < 16; y += .1f)
         {
             PathBehavior.CreateInitialObjects(y - 10);
         }
@@ -18,8 +26,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        pathHasMovedThisFrame = false;
+        updateLocation(); // Used for collision by the path classes
+
         // If they have the "left" key pressed
-        if (Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > -9.8f)
+        if (Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > -12.8f)
         {
             Vector3 position = this.transform.position;
             position.x -= .2f;
@@ -27,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // If they have the "right" key pressed
-        if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < 9.8f)
+        if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < 12.8f)
         {
             Vector3 position = this.transform.position;
             position.x += .2f;
@@ -40,4 +51,10 @@ public class PlayerMovement : MonoBehaviour
             Application.Quit();
         }
 	}
+
+    // Used for collision
+    void updateLocation()
+    {
+        xCoord = this.transform.position.x;
+    }
 }
