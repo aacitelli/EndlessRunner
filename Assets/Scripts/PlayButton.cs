6852b02dce091d1fab90774edbd0e1cbd776ced5
Anden Acitelli <a.acitelli@gmail.com>
2018-06-29 00:09:19 -0400

    Beginning Scene Management Work

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    private void NextScene(string scene)
+    {
+        SceneManager.LoadScene("MainSceneFinal");
+    }
+}
